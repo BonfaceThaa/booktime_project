623d635fe0a15b4c130f4628e9e52283aef55c9d
bonfacethaa <thaabonface@gmail.com>
2020-12-29 22:39:52 +0300

    feat(modules): add app main
    - update settings file environment vars section

--- booktime/booktime/settings.py
+++ booktime/booktime/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = int(os.environ.get("DEBUG", default=0))
+DEBUG = int(os.environ.get("DJANGO_DEBUG", default=0))
 
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
