12cd30af4712206fff8b91ac1a3ff19f1b96ae54
bonfacethaa <thaabonface@gmail.com>
2021-01-01 14:11:21 +0300

    feat(core): add main urls, home and about us templates
    - update main views and urls

--- booktime/main/tests.py
+++ booktime/main/tests.py
@@ -7,4 +7,10 @@ class TestPage(TestCase):
         response = self.client.get("/")
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "home.html")
-        self.assertContains(response, "Hello")
+        self.assertContains(response, "BookTime")
+
+    def test_about_us(self):
+        response = self.client.get("/about-us/")
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "about_us.html")
+        self.assertContains(response, "BookTime")
