8848c36b818eb395040cacaa10754af1521703a5
bonfacethaa <thaabonface@gmail.com>
2021-01-04 16:34:06 +0300

    feat,style(core, modules): update requirements.txt, Dockerfile, docker-compose.tests.yml
    - Add fixtures folder and sample images

--- Dockerfile
+++ Dockerfile
@@ -8,6 +8,9 @@ WORKDIR /usr/src/app
 ENV PYTHONDONTWRITEBYTECODE 1
 ENV PYTHONUNBUFFERED 1
 
+# install pre-requisites
+RUN apk add zlib-dev jpeg-dev gcc musl-dev
+
 # install dependencies
 RUN pip install --upgrade pip
 COPY booktime/requirements.txt .
--- booktime/main/models.py
+++ booktime/main/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 
 
+class ActiveManager(models.Manager):
+    def active(self):
+        return self.filter(active=True)
+
+
 # Create your models here.
 class Product(models.Model):
     name = models.CharField(max_length=32)
@@ -10,6 +15,7 @@ class Product(models.Model):
     active = models.BooleanField(default=True)
     in_stock = models.BooleanField(default=True)
     date_updated = models.DateTimeField(auto_now=True)
+    objects = ActiveManager()
 
 
 class ProductImage(models.Model):
