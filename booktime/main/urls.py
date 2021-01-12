055b2d68af2222cfe4674dab33cd9287dc9d8a73
bonfacethaa <thaabonface@gmail.com>
2021-01-12 23:32:47 +0300

    feat(core): add product list, product detail views and unit tests

--- booktime/main/urls.py
+++ booktime/main/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, DetailView
 
-from main import views
+from main import views, models
 
 urlpatterns = [
     path('about-us/', TemplateView.as_view(template_name="about_us.html")),
     path('', TemplateView.as_view(template_name="home.html")),
     path('contact-us/', views.ContactUsView.as_view(), name="contact_us"),
+    path("product/<slug:slug>/", DetailView.as_view(model=models.Product), name="product",),
+    path("products/<slug:tag>/", views.ProductListView.as_view(), name="products",),
 ]
