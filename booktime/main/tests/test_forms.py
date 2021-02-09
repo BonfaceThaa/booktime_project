d6843ebd7a423050039a6bd2a246a29f7102fc5b
bonfacethaa <thaabonface@gmail.com>
2021-02-09 22:38:14 +0300

    feat(modules): add test for signup form and signup page
    - Change host port for dockerized django app

--- booktime/main/tests/test_forms.py
+++ booktime/main/tests/test_forms.py
@@ -20,3 +20,20 @@ class TestForm(TestCase):
         form = forms.ContactForm({
             'message': "Hi there"})
         self.assertFalse(form.is_valid())
+
+    def test_valid_signup_form_sends_email(self):
+        form = forms.UserCreationForm(
+            {
+                "email": "user@domain.com",
+                "password1": "abcabcabc",
+                "password2": "abcabcabc",
+            }
+        )
+        self.assertTrue(form.is_valid())
+        with self.assertLogs("main.forms", level="INFO") as cm:
+            form.send_mail()
+        self.assertEqual(len(mail.outbox), 1)
+        self.assertEqual(
+            mail.outbox[0].subject, "Welcome to BookTime"
+        )
+        self.assertGreaterEqual(len(cm.output), 1)
