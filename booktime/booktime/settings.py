1b7a1004ea596eff81d8015e7eead934f0a0ba92
bonfacethaa <thaabonface@gmail.com>
2021-02-09 22:01:42 +0300

    feat(core): add user model and register to admin

--- booktime/booktime/settings.py
+++ booktime/booktime/settings.py
@@ -104,6 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# Customer User model
+AUTH_USER_MODEL = "main.User"
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
--- booktime/main/admin.py
+++ booktime/main/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.utils.html import format_html
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+
 from . import models
 
 
@@ -34,6 +36,50 @@ class ProductImageAdmin(admin.ModelAdmin):
 
     def product_name(self, obj):
         return obj.product.name
+
+
+@admin.register(models.User)
+class UserAdmin(DjangoUserAdmin):
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (
+            "Personal info",
+            {"fields": ("first_name", "last_name")},
+        ),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        (
+            "Important dates",
+            {"fields": ("last_login", "date_joined")},
+        ),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email", "password1", "password2"),
+            },
+        ),
+    )
+    list_display = (
+        "email",
+        "first_name",
+        "last_name",
+        "is_staff",
+    )
+    search_fields = ("email", "first_name", "last_name")
+    ordering = ("email",)
 
 
 admin.site.register(models.Product, ProductAdmin)
